be82b9c217dfc45538c3ed1c38927eb7e1dce3c1
Nikita1025 <nikita1025@mail.ru>
2022-08-21 16:46:00 +0300

    Updates

---
+++ precache-manifest.ee38a4c707e471f07857b340616fc483.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a784a30a8ea0215100b70bb4c723a2",
+    "url": "/DzHomeIgnata/index.html"
+  },
+  {
+    "revision": "538492863b17d4addca4",
+    "url": "/DzHomeIgnata/static/css/main.74e36e8e.chunk.css"
+  },
+  {
+    "revision": "92faccda99ac8a3f68f1",
+    "url": "/DzHomeIgnata/static/js/2.6f3c9da1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/DzHomeIgnata/static/js/2.6f3c9da1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538492863b17d4addca4",
+    "url": "/DzHomeIgnata/static/js/main.caf8f335.chunk.js"
+  },
+  {
+    "revision": "7d7a5b637d20a545d4ed",
+    "url": "/DzHomeIgnata/static/js/runtime-main.059e8c37.js"
+  }
+]);
