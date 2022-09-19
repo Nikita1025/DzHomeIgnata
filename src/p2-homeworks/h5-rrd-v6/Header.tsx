2fca8bb14c1cfa841b8294d79df1ca605c6d2be3
Nikita1025 <nikita1025@mail.ru>
2022-09-19 19:59:23 +0300

    Correct DZ5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, {ChangeEvent, useState} from 'react'
 import {NavLink} from "react-router-dom";
 import s from './Header.module.css'
 
 function Header() {
-    return (
-        <div className={s.anim}>
+    let [inp, setInp] = useState(false)
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setInp(e.currentTarget.checked)
+    }
 
-            <NavLink className={s.navlink} to={'/pre-junior'}>pre-junior</NavLink>
-            <NavLink className={s.navlink} to={'/junior'} >junior</NavLink>
-            <NavLink className={s.navlink} to={'/junior+'}>junior+</NavLink>
+
+    return (
+        <div className={s.content}>
+            <div className={`${s.link} ${inp ? s.open : s.close}`}>
+                <NavLink className={s.navlink} to={'/pre-junior'}>pre-junior</NavLink>
+                <NavLink className={s.navlink} to={'/junior'}>junior</NavLink>
+                <NavLink className={s.navlink} to={'/junior+'}>junior+</NavLink>
+                <input type={"checkbox"} onChange={onChangeHandler}/></div>
+
         </div>
     )
 }
